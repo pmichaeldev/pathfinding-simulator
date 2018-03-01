497f871da53d0957fd8341ec9de33911d0e78d07
Philip Michael <phil-mik@hotmail.com>
2018-03-01 18:47:04 -0500

    Begun work on cluster algorithm. Added the clusternode tagging script and a cluster manager.

---
+++ Assets/Scripts/ClusterManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClusterManager : MonoBehaviour {
+
+    #region ABOUT
+    /**
+     * The manager for all cluster nodes.
+     * Builds the lookup table.
+     **/
+    #endregion
+
+    #region VARIABLES
+    // The lookup table for the cluster algorithm
+    public Dictionary<int, Dictionary<int, float>> lookupTable;
+    // lookupTable[1][2] returns a float for the distance between cluster 1 and 2
+    // lookupTable[1][5] would return a float from cluster 1 to 5, despite not having immediate connection
+    // another option List<List<float>>
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ClusterNode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClusterNode : MonoBehaviour {
+    #region ABOUT
+    /**
+     * Cluster identifier for nodes belonging to a cluster.
+     * Clusters are as follows (from top left of Game View):
+     * 1 = top left
+     * 2 = top right
+     * 3 = middle left
+     * 4 = center portion/hallway
+     * 5 = bottom/middle right
+     * 6 = bottom left
+     **/
+    #endregion
+
+    #region VARIABLES
+    [Tooltip("The int ID of the cluster that this node belongs to.")]
+    public int clusterBelongsTo; // Needs to be set in inspector by hand
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
